61b199bf86ddb38f2d91ffae6d9c80c086486374
Santiago Joaquin Diaz <santiagodiaz.arg@gmail.com>
2024-05-08 20:49:37 -0300

    ⚙️ Creación del Recurso de Producto

---
+++ src/main/java/proyecto/dh/resources/product/service/ProductService.java
@@ -0,0 +1,36 @@
+package proyecto.dh.resources.product.service;
+
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import proyecto.dh.exceptions.handler.NotFoundException;
+import proyecto.dh.resources.product.entity.Product;
+import proyecto.dh.resources.product.repository.ProductRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public Product save(Product userObject) { return productRepository.save(userObject); }
+    public Product update(Long id, Product userObject) throws NotFoundException {
+        Product existingProduct = findById(id);
+        modelMapper.map(userObject, existingProduct);
+        return productRepository.save(existingProduct);
+    }
+    public void delete(Long id) throws NotFoundException {
+        Product findProduct = findById(id);
+        productRepository.deleteById(id);
+    }
+    public List<Product> findAll() { return productRepository.findAll(); }
+    public Product findById(Long id) throws NotFoundException {
+        return productRepository.findById(id).orElseThrow(() -> new NotFoundException("El producto no existe"));
+    }
+}
