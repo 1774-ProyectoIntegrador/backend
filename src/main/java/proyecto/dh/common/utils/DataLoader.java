a458f1998f994e91f8af929797992b8cbe5ebf03
Mirty <mirtybmx@gmail.com>
2024-05-11 11:55:06 -0300

    Se han actualizado los datos de carga inicial en DataLoader para incluir información adicional sobre los productos.

--- src/main/java/proyecto/dh/common/utils/DataLoader.java
+++ src/main/java/proyecto/dh/common/utils/DataLoader.java
@@ -6,6 +6,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import proyecto.dh.common.enums.PriceType;
 import proyecto.dh.resources.product.entity.Product;
+import proyecto.dh.resources.product.entity.ProdutDetails;
 import proyecto.dh.resources.product.repository.ProductRepository;
 
 @Component
@@ -19,10 +20,10 @@ public class DataLoader implements ApplicationRunner {
         loadProductData();
     }
     private void loadProductData() {
-        productRepository.save(new Product(null, "Smartphone", "High-end smartphone with 128GB storage", null, 799.99, PriceType.BY_DAY));
-        productRepository.save(new Product(null, "Laptop", "Gaming laptop with high performance specs", null, 1200.00, PriceType.BY_DAY));
-        productRepository.save(new Product(null, "Coffee Maker", "Brews coffee and has programmable features", null, 99.99, PriceType.BY_DAY));
-        productRepository.save(new Product(null, "Bluetooth Speaker", "Portable bluetooth speaker with excellent sound quality", null, 150.00, PriceType.BY_DAY));
-        productRepository.save(new Product(null, "Electric Kettle", "1.7L electric kettle with auto shut-off", null, 35.99, PriceType.BY_DAY));
+        productRepository.save(new Product(null, "Smartphone", "High-end smartphone with 128GB storage", null, 799.99, PriceType.BY_DAY, "Make_one", "Something_one", "Pro", null));
+        productRepository.save(new Product(null, "Laptop", "Gaming laptop with high performance specs", null, 1200.00, PriceType.BY_DAY, "Make_two", "Something_two", "Beginner", new ProdutDetails(null,"Small", "50")));
+        productRepository.save(new Product(null, "Coffee Maker", "Brews coffee and has programmable features", null, 99.99, PriceType.BY_DAY, "Make_three", "Something_three", "Intermediate", null));
+        productRepository.save(new Product(null, "Bluetooth Speaker", "Portable bluetooth speaker with excellent sound quality", null, 150.00, PriceType.BY_DAY, "Make_four", "Something_four", "Advance", new ProdutDetails(null, "Medium", "25")));
+        productRepository.save(new Product(null, "Electric Kettle", "1.7L electric kettle with auto shut-off", null, 35.99, PriceType.BY_DAY, "Make_five", "Something_five", "Beginner", null));
     }
 }
