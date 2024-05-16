be2b5bd61557f91c88bbdca28cfbbd55dd44f250
Santiago Joaquin Diaz <santiagodiaz.arg@gmail.com>
2024-05-15 23:01:08 -0300

    + Updated SwaggerUI and Docker / fix: CORS Origins

---
+++ src/main/java/proyecto/dh/config/OpenApiConfig.java
@@ -0,0 +1,32 @@
+package proyecto.dh.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("RentStudio Backend")
+                        .version("v1.0")
+                        .description("Backend API for RentStudio App"))
+                .servers(List.of(
+                        new Server().url("https://api-digitalhouse.adksolutions.net.ar").description("Servidor de Producción"),
+                        new Server().url("http://localhost:6060").description("Local")
+                ));
+    }
+
+    @Bean
+    public OpenApiCustomizer customerGlobalHeaderOpenApiCustomizer() {
+        return openApi -> openApi.getInfo().setTitle("RentStudio Backend");
+    }
+}
--- src/main/java/proyecto/dh/config/WebConfig.java
+++ src/main/java/proyecto/dh/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*") // Permite solicitudes desde cualquier origen //TODO Modificar en PROD
+                        .allowedOriginPatterns("*") // Permite solicitudes desde cualquier origen
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
