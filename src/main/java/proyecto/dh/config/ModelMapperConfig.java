df54614c793444375d6b8bd4d58004da9d8d2855
Santiago Joaquin Diaz <santiagodiaz.arg@gmail.com>
2024-05-21 17:48:41 -0300

    + La configuración de ModelMapper se ajusta para omitir los campos nulos durante el mapeo.

--- src/main/java/proyecto/dh/config/ModelMapperConfig.java
+++ src/main/java/proyecto/dh/config/ModelMapperConfig.java
@@ -1,6 +1,9 @@
 package proyecto.dh.config;
 
+import org.modelmapper.Condition;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MappingContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +12,19 @@ public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+
+        // Configurar ModelMapper para omitir campos nulos
+        Condition<Object, Object> notNull = new Condition<Object, Object>() {
+            @Override
+            public boolean applies(MappingContext<Object, Object> context) {
+                return context.getSource() != null;
+            }
+        };
+
+        modelMapper.getConfiguration().setPropertyCondition(notNull);
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        return modelMapper;
     }
 }
