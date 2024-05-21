878fb2cb6d0d0b90725bae9a59c3d0bafd4382da
Santiago Joaquin Diaz <santiagodiaz.arg@gmail.com>
2024-05-20 21:25:59 -0300

    + Refactor Code for FileKey S3

--- src/main/java/proyecto/dh/common/service/S3Service.java
+++ src/main/java/proyecto/dh/common/service/S3Service.java
@@ -9,6 +9,8 @@ import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,6 +21,8 @@ import java.util.Base64;
 
 @Service
 public class S3Service {
+
+    private static final Logger logger = LoggerFactory.getLogger(S3Service.class);
 
     private final AmazonS3 s3Client;
     private final String bucketName;
@@ -61,10 +65,29 @@ public class S3Service {
                 .withCannedAcl(CannedAccessControlList.PublicRead);
         s3Client.putObject(putObjectRequest);
 
+        logger.info("File uploaded to S3 with key: {}", key);
+        return key; // Devolver el nombre del archivo generado
+    }
+
+    public String getFileUrl(String key) {
         return s3Client.getUrl(bucketName, key).toString();
     }
 
     public void deleteFile(String key) {
-        s3Client.deleteObject(new DeleteObjectRequest(bucketName, key));
+        try {
+            s3Client.deleteObject(new DeleteObjectRequest(bucketName, key));
+            logger.info("File deleted from S3 with key: {}", key);
+
+            // Verificar si el archivo todavía existe
+            boolean exists = s3Client.doesObjectExist(bucketName, key);
+            if (exists) {
+                logger.error("Failed to delete file from S3 with key: {}", key);
+            } else {
+                logger.info("File successfully deleted from S3 with key: {}", key);
+            }
+        } catch (Exception e) {
+            logger.error("Error deleting file from S3 with key: {}", key, e);
+            throw e;
+        }
     }
 }
