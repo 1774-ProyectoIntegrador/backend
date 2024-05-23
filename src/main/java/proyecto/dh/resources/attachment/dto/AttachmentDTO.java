fb1c2520309b8cc02253728d8a30aa604b476247
Santiago Joaquin Diaz <santiagodiaz.arg@gmail.com>
2024-05-23 20:10:00 -0300

    + modified: Se movieron las funciones para crear, y eliminar archivos al recurso de Attachments para escalabilidad

--- src/main/java/proyecto/dh/resources/attachment/dto/AttachmentDTO.java
+++ src/main/java/proyecto/dh/resources/attachment/dto/AttachmentDTO.java
@@ -1,17 +1,17 @@
 package proyecto.dh.resources.attachment.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class AttachmentDTO {
 
     private Long id;
-
     private String url;
-
     private String fileName;
-
-
 }
