57604d888fb3a9962f2f1ec30a6d366a8c765cec
Santiago Joaquin Diaz <santiagodiaz.arg@gmail.com>
2024-05-23 20:13:09 -0300

    + modified: Se realizaron las modificaciones para mover attachments al recurso correspondientes + modified: Se añadieron validaciones adicionales.

--- src/main/java/proyecto/dh/resources/product/dto/category/ProductCategorySaveDTO.java
+++ src/main/java/proyecto/dh/resources/product/dto/category/ProductCategorySaveDTO.java
@@ -1,13 +1,16 @@
 package proyecto.dh.resources.product.dto.category;
 
-
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProductCategorySaveDTO {
+    @NotBlank
     private String name;
     private String description;
+    @NotBlank
     private String slug;
+    private Long attachmentId;
 }
