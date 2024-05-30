71671e58821fff0a53f122708252e53886d59af1
Santiago Joaquin Diaz <santiagodiaz.arg@gmail.com>
2024-05-29 23:36:02 -0300

    ⚡ fixes: Se arreglo problema que no validaba los Roles

--- src/main/java/proyecto/dh/resources/users/service/UserService.java
+++ src/main/java/proyecto/dh/resources/users/service/UserService.java
@@ -31,7 +31,7 @@ public class UserService {
             throw new BadRequestException("Usuario con email '" + userObject.getEmail() + "' ya existe");
         }
         User userEntity = modelMapper.map(userObject, User.class);
-        userEntity.setRoles(userObject.getRoles() != null ? userObject.getRoles() : Collections.singletonList(Role.ROLE_USER));
+        userEntity.setRoles(userObject.getRoles() != null ? userObject.getRoles() : Collections.singletonList(Role.USER));
         userEntity.setPassword(passwordEncoder.encode(userObject.getPassword()));
         User createdUser = userRepository.save(userEntity);
         return convertToDTO(createdUser);
