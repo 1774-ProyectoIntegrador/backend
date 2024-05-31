e318aa4b3ffd66dcf128390b08c4998ab56076f2
Santiago Joaquin Diaz <santiagodiaz.arg@gmail.com>
2024-05-31 20:40:28 -0300

    ⚙️ configuración swagger

--- src/main/java/proyecto/dh/config/swagger/OpenApiConfig.java
+++ src/main/java/proyecto/dh/config/swagger/OpenApiConfig.java
@@ -2,6 +2,8 @@ package proyecto.dh.config.swagger;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +21,12 @@ public class OpenApiConfig {
                         .title("RentStudio Backend")
                         .version("v1.0")
                         .description("Backend API for RentStudio App"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
                 .servers(List.of(
                         new Server().url("https://apidh.jackmoon.dev").description("Servidor de Producción"),
                         new Server().url("http://localhost:6060").description("Local")
