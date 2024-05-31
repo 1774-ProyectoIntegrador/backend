cbb72dece9b745b8da5bcfe22a6d4002afaae4d7
Santiago Joaquin Diaz <santiagodiaz.arg@gmail.com>
2024-05-31 20:42:14 -0300

    ⚙️ cambio de endpoints privados y públicos y nombre de clases

--- src/main/java/proyecto/dh/resources/auth/controller/AuthController.java
+++ src/main/java/proyecto/dh/resources/auth/controller/AuthController.java
@@ -1,5 +1,6 @@
 package proyecto.dh.resources.auth.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 import proyecto.dh.resources.auth.dto.AuthenticationRequestDto;
 import proyecto.dh.resources.auth.dto.AuthenticationResponseDto;
 import proyecto.dh.resources.auth.service.AuthService;
+import proyecto.dh.resources.product.service.ProductService;
 
 @RestController
 @RequestMapping("/auth")
+@Tag(name = "Auth Controller", description = "Controlador para ingresar como usuario")
 public class AuthController {
 
     @Autowired
