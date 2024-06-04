ab5ad8667c772c40c51e12fbc2c8dc99e0440faf
Santiago Joaquin Diaz <santiagodiaz.arg@gmail.com>
2024-06-03 22:55:40 -0300

    ⚙️ Cambios en Docker Files y Compose

--- start_containers.sh
+++ start_containers.sh
@@ -22,7 +22,7 @@ fi
 
 # Stop and remove existing containers
 echo "[JACKMOON-DEV] Stopping and removing existing containers..."
-docker-compose down
+docker-compose -f "compose.prod.yml" down
 sleep 10  # Adding a delay to ensure proper shutdown
 
 if [ $? -ne 0 ]; then
@@ -32,7 +32,7 @@ fi
 
 # Build the containers
 echo "[JACKMOON-DEV] Building the containers..."
-docker-compose build
+docker-compose -f "compose.prod.yml" build
 
 if [ $? -ne 0 ]; then
     echo "[JACKMOON-DEV] Error during container build."
@@ -41,7 +41,7 @@ fi
 
 # Start the containers in the background
 echo "[JACKMOON-DEV] Starting the containers..."
-docker-compose up -d
+docker-compose -f "compose.prod.yml" up -d
 
 if [ $? -ne 0 ]; then
     echo "[JACKMOON-DEV] Error while starting the containers."
