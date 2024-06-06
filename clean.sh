7f7feb33a32834e87f91d0bf643551aec6ea20f4
Santiago Joaquin Diaz <santiagodiaz.arg@gmail.com>
2024-06-06 03:27:13 -0300

    ⚙️ misc: Se cambio el script de limpieza

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Esperar a que los contenedores estén levantados (puedes ajustar el tiempo de espera según tus necesidades)
-sleep 80
+sleep 60
 
 # Eliminar imágenes colgantes (dangling)
 docker image prune -f
