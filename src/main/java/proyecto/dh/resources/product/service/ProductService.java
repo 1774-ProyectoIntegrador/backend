591150a0655ff927972e72a7235cad8e6fda881b
Santiago Joaquin Diaz <santiagodiaz.arg@gmail.com>
2024-06-10 16:32:14 -0300

    ⚡ documentación de codigo

--- src/main/java/proyecto/dh/resources/product/service/ProductService.java
+++ src/main/java/proyecto/dh/resources/product/service/ProductService.java
@@ -13,12 +13,10 @@ import proyecto.dh.resources.product.entity.Product;
 import proyecto.dh.resources.product.entity.ProductCategory;
 import proyecto.dh.resources.product.entity.ProductFeature;
 import proyecto.dh.resources.product.repository.ProductCategoryRepository;
-import proyecto.dh.resources.product.repository.ProductFeatureRepository;
 import proyecto.dh.resources.product.repository.ProductRepository;
 import proyecto.dh.resources.product.repository.ProductSearchRepository;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -40,6 +38,14 @@ public class ProductService {
         this.productSearchRepository = productSearchRepository;
     }
 
+    /**
+     * Guarda un nuevo producto.
+     *
+     * @param productSaveDTO DTO con la información del producto a guardar.
+     * @return El producto guardado convertido a DTO.
+     * @throws NotFoundException Si no se encuentra la categoría.
+     * @throws BadRequestException Si el nombre del producto ya existe.
+     */
     @Transactional
     public ProductDTO save(ProductSaveDTO productSaveDTO) throws NotFoundException, BadRequestException {
         if (productRepository.existsByName(productSaveDTO.getName())) {
@@ -54,6 +60,15 @@ public class ProductService {
         return convertToDTO(savedProduct);
     }
 
+    /**
+     * Actualiza un producto existente.
+     *
+     * @param id ID del producto a actualizar.
+     * @param productUpdateDTO DTO con la información del producto a actualizar.
+     * @return El producto actualizado convertido a DTO.
+     * @throws NotFoundException Si no se encuentra el producto o la categoría.
+     * @throws BadRequestException Si hay algún problema con las características o los archivos adjuntos.
+     */
     @Transactional
     public ProductDTO updateProduct(Long id, ProductUpdateDTO productUpdateDTO) throws NotFoundException, BadRequestException {
         Product existingProduct = findByIdEntity(id);
@@ -67,6 +82,12 @@ public class ProductService {
         return convertToDTO(updatedProduct);
     }
 
+    /**
+     * Elimina un producto por su ID.
+     *
+     * @param id ID del producto a eliminar.
+     * @throws NotFoundException Si no se encuentra el producto.
+     */
     @Transactional
     public void delete(Long id) throws NotFoundException {
         Product findProduct = findByIdEntity(id);
@@ -74,17 +95,37 @@ public class ProductService {
         productRepository.delete(findProduct);
     }
 
+    /**
+     * Obtiene todos los productos.
+     *
+     * @return Lista de productos convertidos a DTO.
+     */
     public List<ProductDTO> findAll() {
         return productRepository.findAll().stream()
                 .map(this::convertToDTO)
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Busca un producto por su ID.
+     *
+     * @param id ID del producto a buscar.
+     * @return El producto encontrado convertido a DTO.
+     * @throws NotFoundException Si no se encuentra el producto.
+     */
     public ProductDTO findById(Long id) throws NotFoundException {
         Product productSearched = findByIdEntity(id);
         return convertToDTO(productSearched);
     }
 
+    /**
+     * Busca productos por texto y categoría.
+     *
+     * @param searchText Texto a buscar.
+     * @param categoryId ID de la categoría a buscar.
+     * @return Lista de productos encontrados convertidos a DTO.
+     * @throws NotFoundException Si no se encuentran productos.
+     */
     @Transactional(readOnly = true)
     public List<ProductDTO> searchProducts(String searchText, Long categoryId) throws NotFoundException {
         return productSearchRepository.searchProducts(searchText, categoryId);
@@ -96,7 +137,7 @@ public class ProductService {
         product.setCategory(category);
     }
 
-    private void setProductAttachments(Product product, List<Long> attachmentsIds) throws BadRequestException, NotFoundException {
+    private void setProductAttachments(Product product, List<Long> attachmentsIds) throws BadRequestException {
         if (attachmentsIds != null) {
             List<Attachment> attachments = new ArrayList<>();
             for (Long attachmentId : attachmentsIds) {
@@ -109,19 +150,17 @@ public class ProductService {
         }
     }
 
-    private void setProductFeatures(Product product, List<ProductFeatureSaveDTO> features) throws NotFoundException, BadRequestException {
-        Set<ProductFeature> featureSet = new HashSet<>();
-
-        if (features != null) {
-            for (ProductFeatureSaveDTO featureSaveDTO : features) {
-                ProductFeature feature = modelMapper.map(featureSaveDTO, ProductFeature.class);
-                if (feature.getProduct() == null) {
-                    feature.setProduct(new HashSet<>());
-                }
-                feature.getProduct().add(product);
-                featureSet.add(feature);
-            }
-        }
+    private void setProductFeatures(Product product, List<ProductFeatureSaveDTO> features) {
+        Set<ProductFeature> featureSet = features.stream()
+                .map(featureSaveDTO -> modelMapper.map(featureSaveDTO, ProductFeature.class))
+                .peek(feature -> {
+                    if (feature.getProduct() == null) {
+                        feature.setProduct(Set.of(product));
+                    } else {
+                        feature.getProduct().add(product);
+                    }
+                })
+                .collect(Collectors.toSet());
 
         product.setProductFeatures(featureSet);
     }
@@ -135,18 +174,14 @@ public class ProductService {
     }
 
     @Transactional
-    public void updateAttachments(Product product, List<Long> attachmentsIds) throws BadRequestException, NotFoundException {
+    public void updateAttachments(Product product, List<Long> attachmentsIds) throws BadRequestException {
         if (attachmentsIds != null) {
             product.getAttachments().clear();
-            for (Long attachmentId : attachmentsIds) {
-                Attachment attachment = attachmentService.findById(attachmentId);
-                attachmentService.validateFileTypeImages(attachment);
-                product.addAttachment(attachment);
-            }
+            setProductAttachments(product, attachmentsIds);
         }
     }
 
-    private void updateFeatures(Product product, List<ProductFeatureSaveDTO> features) throws NotFoundException, BadRequestException {
+    private void updateFeatures(Product product, List<ProductFeatureSaveDTO> features) {
         product.getProductFeatures().clear();
         setProductFeatures(product, features);
     }
@@ -160,8 +195,7 @@ public class ProductService {
         ProductDTO productDTO = modelMapper.map(product, ProductDTO.class);
 
         if (product.getCategory() != null) {
-            CategoryDTO categoryDTO = modelMapper.map(product.getCategory(), CategoryDTO.class);
-            productDTO.setCategory(categoryDTO);
+            productDTO.setCategory(modelMapper.map(product.getCategory(), CategoryDTO.class));
         }
 
         if (product.getAttachments() != null) {
