97b36788256824baff1e9b78414599030830f2cd
Mirty <mirtybmx@gmail.com>
2024-06-17 17:54:06 -0300

    Se modificó el método findByUserAndProduct a findByUserIdAndProductId

--- src/main/java/proyecto/dh/resources/favorites/repository/ProductFavoriteRepository.java
+++ src/main/java/proyecto/dh/resources/favorites/repository/ProductFavoriteRepository.java
@@ -3,13 +3,13 @@ package proyecto.dh.resources.favorites.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import proyecto.dh.resources.favorites.entity.ProductFavorite;
-import proyecto.dh.resources.product.entity.Product;
 import proyecto.dh.resources.users.entity.User;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductFavoriteRepository extends JpaRepository<ProductFavorite, Long> {
     List<ProductFavorite> findByUser(User user);
-    ProductFavorite findByUserAndProduct(User user, Product product);
+    Optional<ProductFavorite> findByUserIdAndProductId(Long userId, Long productId);
 }
