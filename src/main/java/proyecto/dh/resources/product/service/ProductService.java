8d7324cef463e12a0d56c3d58a7ae6995f190841
Mirty <mirtybmx@gmail.com>
2024-06-30 16:58:55 -0300

    Se modificaron los métodos save, setReservation y convertToDTO.

--- src/main/java/proyecto/dh/resources/product/service/ProductService.java
+++ src/main/java/proyecto/dh/resources/product/service/ProductService.java
@@ -65,7 +65,7 @@ public class ProductService {
         setProductFeatures(product, productSaveDTO.getFeatures());
         setProductPolicies(product, productSaveDTO.getPolicies());
         setProductFavorites(product, productSaveDTO.getFavorites());
-        setReservations(product, productSaveDTO.getReservations());
+        setReservations(product, productSaveDTO.getReservation());
 
 
 
@@ -253,20 +253,13 @@ public class ProductService {
         product.setFavorites(favoriteSet);
     }
 
-    private void setReservations(Product product, List<ReservationSaveDTO> reservationSaveDTOS) {
-        Set<Reservation> reservationSet = Optional.ofNullable(reservationSaveDTOS)
-                .orElseGet(Collections::emptyList)
-                .stream()
-                .map(reservationSaveDTO -> modelMapper.map(reservationSaveDTO, Reservation.class))
-                .peek(reservation -> {
-                    if(reservation.getProduct() == null){
-                        reservation.setProduct(Set.of(product));
-                    } else {
-                        reservation.getProduct().add(product);
-                    }
-                })
-                .collect(Collectors.toSet());
-        product.setReservations(reservationSet);
+    private void setReservations(Product product, ReservationSaveDTO reservationSaveDTO) {
+
+        if (reservationSaveDTO != null) {
+            Reservation reservation = modelMapper.map(reservationSaveDTO, Reservation.class);
+            reservation.setProduct(product);
+            product.setReservation(reservation);
+        }
     }
 
     private void updateCategory(Product product, Long categoryId) throws NotFoundException {
@@ -332,11 +325,10 @@ public class ProductService {
             productDTO.setFavorites(favorites);
         }
 
-        if(product.getReservations() != null) {
-            List<ReservationDTO> reservations = product.getReservations().stream()
-                    .map(reservation -> modelMapper.map(reservation, ReservationDTO.class))
-                    .collect(Collectors.toList());
-            productDTO.setReservations(reservations);
+        if(product.getReservation() != null) {
+            ReservationDTO reservationDTO = modelMapper.map(product.getReservation(), ReservationDTO.class);
+            productDTO.setReservation(reservationDTO);
+
         }
 
         return productDTO;
